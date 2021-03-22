f14f7c175ac6a733a29741f4c54d89b05fab2a39
Sun <sunping1415926@gmail.com>
2021-03-22 14:45:33 +0800

    add basic security checks and enhance functionality

--- bin/audit.rb
+++ bin/audit.rb
@@ -6,3 +6,12 @@ puts "Basic system check functionality"
 # Basic system info gathering
 hostname = `hostname`.strip
 puts "System: #{hostname}"
+
+# Add some basic security checks
+puts "Checking SSH configuration..."
+ssh_root = `grep PermitRootLogin /etc/ssh/sshd_config`.strip
+puts "SSH Root Access: #{ssh_root}"
+
+puts "Checking for updates..."
+updates = `apt list --upgradable 2>/dev/null | wc -l`.strip.to_i - 1
+puts "Available updates: #{updates}"
